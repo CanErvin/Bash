a72857515f530cd7d7b37cdd8b7cab3533acb90e
CanErvin <143337588+CanErvin@users.noreply.github.com>
2023-09-12 20:53:39 +0200

    Create myscripte.sh

---
+++ myscripte.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#To execute the script, do not forget to make script executable by chmod +x myscript.sh
+#to execute the script, type ./myscript.sh
+#First line of script starts with hashbang a.k.a shabang.
+
+ls
+pwd
+
+
