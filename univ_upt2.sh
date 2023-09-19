f9e2b9872879e035758ebb89663706362ea4419a
CanErvin <143337588+CanErvin@users.noreply.github.com>
2023-09-19 22:46:11 +0200

    Add files via upload

---
+++ univ_upt2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+release_file=/etc/os-release
+
+if grep -q "Arch" $release_file
+then
+
+        # The host is based on Arch, run the pacman update command
+        sudo pacman -Syu
+fi
+
+if grep -q "Ubuntu" $release_file
+then
+
+        # The host is based on Debian or Ubuntu,
+        # Run the apt version of the command
+        sudo apt update
+        sudo apt dist-uprage
+fi
+
+if grep -q "Mint" $release_file
+then
+
+        # The host is based on Debian or Ubuntu,
+        # Run the apt version of the command
+        sudo apt update
+        sudo apt dist-uprage
+fi
+
+if grep -q "Pop" $release_file
+then
+
+        # The host is based on Debian or Ubuntu,
+        # Run the apt version of the command
+        sudo apt update
+        sudo apt dist-uprage
+fi
